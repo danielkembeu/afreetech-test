a7d9bfcf811fe2d17f2db27f378baf9f38b5c56b
danielkembeu <kembeudaniel2@gmail.com>
2024-04-18 16:59:23 +0100

    Map search optimized

--- src/lib/components/map/Map.jsx
+++ src/lib/components/map/Map.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import Nav from '../navbar/Nav';
 import markers from '../../utils/markers';
@@ -7,38 +7,31 @@ import markers from '../../utils/markers';
 
 export default function Map() {
 
-  const [location, setLocation] = React.useState();
+  const [location, setLocation] = React.useState(markers[0]);
 
-
-  const [input, setInput] = React.useState(markers[0]);
+  const [input, setInput] = React.useState();
   const [searchResults, setSearchResults] = React.useState([]);
 
-  React.useEffect(() => {
-    setLocation(markers[0]);
-    console.log(markers[0]);
+  const handleSearch = (event) => {
+    setInput(event.target.value);
+    const mappings = markers.filter(item => item.city.toLowerCase().includes(event.target.value));
+    setSearchResults(mappings);
 
-  }, []);
-
-
-  const handleSearch = () => {
-    setSearchResults(() => {
-      markers.filter(item => item.city.toLowerCase().includes(input.toLowerCase()));
-
-      console.log;
-    });
+    console.log(mappings);
   };
 
 
   const selectLocation = id => {
-    setLocation(markers.find(m => m.id === id));
+    const newPos = markers.find(m => m.id === id);
+    setLocation(newPos);
   }
 
   return (
     <main className='h-screen w-screen'>
-      <Nav value={input} onValueChange={setInput} onFormSubmit={handleSearch} onButtonClick={selectLocation} />
-      <div className='my-10 z-100 bg-gray-100 rounded-lg'>
+      <Nav value={input} onValueChange={handleSearch} onButtonClick={selectLocation} />
+      <div className='mt-10 absolute z-100 bg-white w-[200px] h-[200px] rounded-lg'>
         {
-          searchResults !== '' && searchResults.map(item => {
+          searchResults && searchResults.map(item => {
             <button onClick={() => onButtonClicked(item.id)} key={item.id}>{item.city}</button>
           })
         }
@@ -48,17 +41,30 @@ export default function Map() {
 
       {
         location ? (
-          <MapContainer center={location.coords} zoom={13} className='h-full' >
+          <MapContainer center={location.coords} zoom={8} className='h-full' >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={location.coords}>
-              <Popup>
-                <p className='font-bold text-gray-600'>Heyyy there friend ! 🖐🏾😅</p>
-              </Popup>
-            </Marker>
+
+            {
+              searchResults.length > 0 ? (
+                searchResults.map(item => (
+                  <Marker key={item.id} position={item.coords}>
+                    <Popup>
+                      <p className='font-bold text-gray-600'>{item.content}</p>
+                    </Popup>
+                  </Marker>
+                ))
+              ) : (
+                <Marker position={location.coords}>
+                  <Popup>
+                    <p className='font-bold text-gray-600'>{location.content}</p>
+                  </Popup>
+                </Marker>
+              )
+            }
           </MapContainer >
         ) : (
-          <div className='h-screen w-screen items-center justify-center'>
-            <h3 className='text-4xl font-bold text-gray-600'>Waiting for the authorization...</h3>
+          <div className='h-screen w-screen flex items-center justify-center'>
+            <h3 className='text-4xl font-bold text-gray-600 animate-pulse'>Loading...</h3>
           </div>
         )
       }
--- src/lib/components/navbar/Nav.jsx
+++ src/lib/components/navbar/Nav.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { LuSearch } from 'react-icons/lu';
 
-export default function Nav({ value, onValueChange, onFormSubmit }) {
+export default function Nav({ value, onValueChange }) {
 
 
   const search = (event) => {
     event.preventDefault();
-
-    onFormSubmit();
   };
 
   return (
@@ -16,7 +14,7 @@ export default function Nav({ value, onValueChange, onFormSubmit }) {
         <h3 className='text-2xl text-green-500 font-bold'>AfreeTech</h3>
 
         <form action="" onSubmit={search} className='flex items-center justify-between bg-gray-200 rounded-full mx-12 py-2 px-4 w-[400px]'>
-          <input value={value} onChange={(e) => onValueChange(e.target.value)} placeholder='Search area...' className='h-4 bg-transparent focus:outline-none' />
+          <input value={value} onChange={(e) => onValueChange(e)} placeholder='Search area...' className='w-full h-4 bg-transparent focus:outline-none' />
           <button type='submit' className='h-[38px] w-[38px] rounded-full flex items-center justify-center bg-gray-100'>
             <LuSearch size={18} className='text-gray-400' />
           </button>
